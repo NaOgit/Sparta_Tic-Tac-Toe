f9719b00d58b3791c054e49ce0d97b979bac0268
NaO <noukongka@spartaglobal.com>
2018-08-12 12:49:23 +0100

    A drwa check is added

--- main.js
+++ main.js
@@ -2,45 +2,54 @@ $(function() {
 //Declaring a variable to allowed turns
 var playerTurn = true;
 //Gameplay
+var turns = 0;
 
-
-$('[index]').click(function() {
+$('[index]').on("click", function() {
   //Prevent overlapping gameplay
   if ($(this).hasClass("X") || $(this).hasClass("O")) {
     alert("Position has been taken! Try again.")
-  }
+  }if (turns < 8) {
     //X will appear when it's Player 1's turn
     if (playerTurn == true) {
-    $(this).addClass("X").html("X");
-    $("#message").html("It's Player 2's turn");
-    //Player 1's turn has ended
-    playerTurn = false;
-    //Check for winning combination inputs
-    checkWin($('#box0'),('#box1'),('#box2'),"Player 1");
-    checkWin($('#box3'),('#box4'),('#box5'),"Player 1");
-    checkWin($('#box6'),('#box7'),('#box8'),"Player 1");
-    checkWin($('#box0'),('#box4'),('#box8'),"Player 1");
-    checkWin($('#box2'),('#box4'),('#box6'),"Player 1");
-    checkWin($('#box0'),('#box3'),('#box6'),"Player 1");
-    checkWin($('#box1'),('#box4'),('#box7'),"Player 1");
-    checkWin($('#box2'),('#box5'),('#box8'),"Player 1");
+      $(this).addClass("X").html("X");
+      $("#message").html("It's Player 2's turn");
+      //Player 1's turn has ended
+      playerTurn = false;
+      turns++;
+      //Check for winning combination inputs
+      checkWin($('#box0'),('#box1'),('#box2'),"Player 1");
+      checkWin($('#box3'),('#box4'),('#box5'),"Player 1");
+      checkWin($('#box6'),('#box7'),('#box8'),"Player 1");
+      checkWin($('#box0'),('#box4'),('#box8'),"Player 1");
+      checkWin($('#box2'),('#box4'),('#box6'),"Player 1");
+      checkWin($('#box0'),('#box3'),('#box6'),"Player 1");
+      checkWin($('#box1'),('#box4'),('#box7'),"Player 1");
+      checkWin($('#box2'),('#box5'),('#box8'),"Player 1");
+
     //O will appear when it's Player 2's turn
   } else if (playerTurn == false) {
-    $(this).addClass("O").html("O");
-    $("#message").html("It's Player 1's turn");
-    //Player 2's turn has ended
-    playerTurn = true;
-    //Check for winning combination inputs
-    checkWin($('#box0'),('#box1'),('#box2'),"Player 2");
-    checkWin($('#box3'),('#box4'),('#box5'),"Player 2");
-    checkWin($('#box6'),('#box7'),('#box8'),"Player 2");
-    checkWin($('#box0'),('#box4'),('#box8'),"Player 2");
-    checkWin($('#box2'),('#box4'),('#box6'),"Player 2");
-    checkWin($('#box0'),('#box3'),('#box6'),"Player 2");
-    checkWin($('#box1'),('#box4'),('#box7'),"Player 2");
-    checkWin($('#box2'),('#box5'),('#box8'),"Player 2");
-  } 
+      $(this).addClass("O").html("O");
+      $("#message").html("It's Player 1's turn");
+      //Player 2's turn has ended
+
+      playerTurn = true;
+      turns++;
+      //Check for winning combination inputs
+      checkWin($('#box0'),('#box1'),('#box2'),"Player 2");
+      checkWin($('#box3'),('#box4'),('#box5'),"Player 2");
+      checkWin($('#box6'),('#box7'),('#box8'),"Player 2");
+      checkWin($('#box0'),('#box4'),('#box8'),"Player 2");
+      checkWin($('#box2'),('#box4'),('#box6'),"Player 2");
+      checkWin($('#box0'),('#box3'),('#box6'),"Player 2");
+      checkWin($('#box1'),('#box4'),('#box7'),"Player 2");
+      checkWin($('#box2'),('#box5'),('#box8'),"Player 2");
+    }
+  } else {
+      $("#message").html("It's a draw!")
+  }
+
 });
+
 
 //Function for chechking winning patterns is declared
 function checkWin(box1, box2, box3, player) {
@@ -61,12 +70,14 @@ function checkWin(box1, box2, box3, player) {
     }
   }
 }
+
 //Clear the grid after a winner is declared
 $('#restart').click(function() {
   //All Xs and Os are removed
   $('td').removeClass("O X").html("");
+  turns = 0;
   //The winner from the last round will start
-  $("#message").html("Winner starts!")
+  $("#message").html("Let's play!")
 });
 
 });
